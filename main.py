95b3a04e42632f22dcc46f3b7e7b9e4210144f34
Chris Bay <chris@chrisbay.net>
2016-08-06 08:35:56 -0500

    movie of the day, hardcoded

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import webapp2
 
 class Index(webapp2.RequestHandler):
+
+    def getRandomMovie(self):
+
+        # TODO: make an array with at least 5 movie titles
+
+        # TODO: randomly choose one of the movies, and return it
+
+        return "The Big Lebowski"
+
     def get(self):
-        self.response.write('Hello, Summer of Code!')
+        movie = self.getRandomMovie()
+
+        # build the response string
+        response = "<h1>Movie of the Day</h1>"
+        response += "<ul><li>" + movie + "</li></ul>"
+
+        self.response.write(response)
 
 app = webapp2.WSGIApplication([
     ('/', Index)
