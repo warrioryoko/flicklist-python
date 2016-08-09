10e99b21c1b4c51476c455be9c47f94ba5f50e25
jesseilev <jesseilev@gmail.com>
2016-08-09 17:46:50 -0500

    add task about choosing tomorrow's movie

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ class Index(webapp2.RequestHandler):
         response = "<h1>Movie of the Day</h1>"
         response += "<p>" + movie + "</p>"
 
+        # TODO: pick a different random movie, and display it under
+        # the heading "<h1>Tommorrow's Movie</h1>"
+
         self.response.write(response)
 
 app = webapp2.WSGIApplication([
