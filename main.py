b0341a5e36e3ee2152961524b934c6ef3ab716c8
warrioryoko <miangpellegri@gmail.com>
2017-01-19 19:55:40 -0600

    Finished studio. Checks that both random movies do not match.

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import webapp2
+import random
 
 class Index(webapp2.RequestHandler):
 
     def getRandomMovie(self):
 
         # TODO: make a list with at least 5 movie titles
+        movie_list =['Pulp Fiction', 'Kill Bill', 'The Matrix', 'Dr. Strange', 'Star Wars: Rogue One']
 
         # TODO: randomly choose one of the movies, and return it
-
-        return "The Big Lebowski"
+        rand_selection = random.choice(movie_list)
+        return rand_selection
 
     def get(self):
         # choose a movie by invoking our new function
@@ -21,6 +23,12 @@ class Index(webapp2.RequestHandler):
         # TODO: pick a different random movie, and display it under
         # the heading "<h1>Tommorrow's Movie</h1>"
 
+        next_movie = self.getRandomMovie()
+
+        if next_movie == movie:
+            next_movie = self.getRandomMovie()
+        content += "<h1>Tomorrow's Movie</h1>"
+        content += "<p>" + next_movie + "</p>"
         self.response.write(content)
 
 app = webapp2.WSGIApplication([
